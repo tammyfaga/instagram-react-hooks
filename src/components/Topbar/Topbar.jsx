ebfd526cc9b8751220ad9c0c98097a87d323acbf
Tammy Fagá <tammyfaga@yahoo.com.br>
2020-06-28 22:21:33 -0300

    tarefa: adiciona Topbar

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -9,6 +9,23 @@ import './Topbar.scss';
 const Topbar = () => (
   <header className="topbar" data-testid="topbar">
     <div className="container">
+      <Link to='/' className="topbar__logo">
+        <LogoSvg />
+      </Link>
+      <div className="topbar__group">
+        <button className="topbar__icon">
+          <Link to='/users'>
+            <i className="fas fa-users"></i>
+            <span>Usuários</span>
+          </Link>
+        </button>
+        <button className="topbar__icon">
+          <Link to='/newusers'>
+            <i className="fas fa-user-plus"></i>
+            <span>Nova Conta</span>
+          </Link>
+        </button>
+      </div>
     </div>
   </header>
 );
