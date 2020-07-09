95159775a9ec22632a698948949d58e14c45e890
Tammy Fagá <tmata@recordtv.com.br>
2020-07-08 21:54:39 -0300

    tarefa: fetch api stories

--- src/routes/UsersRoute/UsersRoute.jsx
+++ src/routes/UsersRoute/UsersRoute.jsx
@@ -3,8 +3,25 @@ import React, { useState, useEffect } from 'react';
 import UsersList from '../../containers/UsersList/UsersList';
 
 const UsersRoute = () => {
+  const [users, setUsers] = useState({ users: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const responseUsers = await fetch(
+        'https://5e7d0266a917d70016684219.mockapi.io/api/v1/stories',
+      );
+
+      const dataUsers = await responseUsers.json();
+ 
+      setUsers({ dataUsers });
+    }
+    fetchData();
+  }, []);
+
+  console.log('users', users);
   return (
     <div className="container" data-testid="users-route">
+      <UsersList users={users} />
     </div>
   );
 };
